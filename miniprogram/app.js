53419318660bbb4f72e0c59b5031f2b5d64f3247
ChiangYintso <392711804@qq.com>
2019-07-23 12:09:07 +0800

    refactor: add global layout css

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,3 +1,14 @@
 //app.js
 App({
+  onLaunch: function () {
+    let appInstance = this
+    wx.getUserInfo({
+      success: function (res) {
+        appInstance.globalData.userInfo = res.userInfo
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
 })
--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,10 +1,46 @@
 //index.js
-const app = getApp()
+let app = getApp()
 
 Page({
+  onShow: function () {
+    this.getUserInfo()
+  },
+  data: {
+    intro: '点击头像授权登录',
+    avatarUrl: '../../static/avatar.svg'
+  },
+  getUserInfo: function (event) {
+    if (app.globalData.userInfo === null) {
+      wx.getUserInfo({
+        success: (res) => {
+          app.globalData.userInfo = res.userInfo
+          if (event !== undefined) {
+            this.navigateTo(event)
+          }
+          this.updateUserInfo()
+        }
+      })
+    } else {
+      if (event!==undefined) {
+        this.navigateTo(event)
+      }
+      this.updateUserInfo()
+    }
+  },
   navigateTo: function (event) {
-    wx.navigateTo({
-      url: event.currentTarget.dataset.url,
-    })
+    if (event.currentTarget.dataset.url) {
+      wx.navigateTo({
+        url: event.currentTarget.dataset.url
+      })
+    }
+  },
+  updateUserInfo: function () {
+    if(app.globalData.userInfo){
+      console.log(app.globalData.userInfo)
+      this.setData({
+        intro: app.globalData.userInfo.nickName,
+        avatarUrl: app.globalData.userInfo.avatarUrl
+      })
+    }
   }
 })
