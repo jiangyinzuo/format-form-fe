4c5001371cc032dc219b4ef91e6ece9afee41993
jiang yinzuo <392711804@qq.com>
2019-07-26 11:02:36 +0800

    feat; wx.login

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,10 +1,38 @@
 //app.js
+import {config} from './config.js'
+function login() {
+  wx.login({
+    success: (res) => {
+      if (res.code) {
+        console.log('login successfully')
+        console.log(res)
+        wx.request({
+          url: config.API_BASE_URL + '/wx_login',
+          data: {
+            code: res.code,
+          },
+          method: 'POST',
+          success: function (res) {
+            console.log(res)
+          }
+        })
+      } else {
+        console.log('login failed')
+      }
+    },
+    fail: (err) => {
+      console.log('error!')
+      console.log(err)
+    }
+  })
+}
+
 App({
   onLaunch: function () {
-    let appInstance = this
+    login()
     wx.getUserInfo({
-      success: function (res) {
-        appInstance.globalData.userInfo = res.userInfo
+      success: (res) => {
+        this.globalData.userInfo = res.userInfo
       }
     })
   },
