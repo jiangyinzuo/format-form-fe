89e72fbae6586ea5aa54b8b37c4823753af84ece
jiang yinzuo <392711804@qq.com>
2019-08-13 13:53:15 +0800

    feat: rewrite form validate

--- miniprogram/components/dashBoard/select/select.js
+++ miniprogram/components/dashBoard/select/select.js
@@ -6,7 +6,7 @@ Component({
   properties: {
     radioList: {
       type: Array,
-      value: []
+      value: ['', '']
     }
   },
 
@@ -14,34 +14,41 @@ Component({
    * 组件的初始数据
    */
   data: {
-    itemDesc: '',
-    order: ['A. ', 'B. ', 'C. ', 'D. ', 'E. ', 'F. ', 'G. ', 'H. ', 'I. ', 'J. ', 'K. ', 'L. ', 'M. ', 'N. ', 'O. ', 'P. ', 'Q. ', 'R. ', 'S. ', 'T. ', 'U. ', 'V. ', 'W. ', 'X. ', 'Y. ', 'Z. '],
+    order: ['A. ', 'B. ', 'C. ', 'D. ', 'E. ', 'F. ', 'G. ', 'H. ', 'I. ', 'J. ', 'K. ', 'L. ', 'M. ', 'N. ', 'O. ', 'P. ', 'Q. ', 'R. ', 'S. ', 'T. '],
     showAddBtn: true
   },
 
   methods: {
     addRadio() {
-      if (this.data.itemDesc) {
-        this.properties.radioList.push(this.data.itemDesc)
-        this.setData({
-          itemDesc: '',
-          radioList: this.properties.radioList
+      if (this.properties.radioList.length >= 20) {
+        wx.showToast({
+          title: '选项不能多于20个',
+          icon: 'none'
         })
-      }
-      if (this.properties.radioList.length >= 26) {
+      } else {
+        this.properties.radioList.push('')
         this.setData({
-          showAddBtn: false
+          radioList: this.properties.radioList
         })
       }
     },
     onDescInput(event) {
-      this.data.itemDesc = event.detail.value
-    },
+      console.log(event)
+      let idx = event.target.dataset.idx
+      this.properties.radioList[idx] = event.detail.value
+    }, 
     delRadio(event) {
-      this.data.radioList.splice(event.target.dataset.idx, 1)
-      this.setData({
-        radioList: this.data.radioList
-      })
+      if (this.data.radioList.length <= 2) {
+        wx.showToast({
+          title: '选项不能少于2个',
+          icon: 'none'
+        })
+      } else {
+        this.data.radioList.splice(event.target.dataset.idx, 1)
+        this.setData({
+          radioList: this.data.radioList
+        })
+      }
     }
   }
 })
